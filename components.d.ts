8fa66f62c7225994252062ca5eaaa95a0b217212
mkitzmann <github@kitzmann.me>
2024-04-11 17:03:38 +0200

    add ListingCard and grid

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,8 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    Components: typeof import('./src/components/index.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    ListingCard: typeof import('./src/components/ListingCard.vue')['default']
   }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,12 @@
-<template><div>Index2</div></template>
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col v-for="n in 6" :key="n" cols="12" sm="6" md="4" lg="3">
+        <ListingCard></ListingCard>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
 <script setup lang="ts">
 //
